afc396612fe36731a16dc2ba0f3c365200219a22
FabrizioMisseri <misserifabrizio@gmail.com>
2022-12-15 16:44:40 +0100

    add main-partial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts/app')
 
 @section('content')
-    ciao sono main
+    <div class="home">
+
+        <div class="container">
+            <div class="row">
+
+                @foreach ($db as $item)
+                    <div class="col-2 p-4">
+
+                        <div class="card">
+                            <figure>
+                                <img src=" {{ $item['thumb'] }} " alt="">
+                            </figure>
+
+                            <p class="p-2">
+                                {{ $item['title'] }}
+                            </p>
+                        </div>
+
+                    </div>
+                @endforeach
+
+            </div>
+        </div>
+
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
+
+
     $data = [
         'navbarArray' => [
             'characters',
@@ -28,6 +30,8 @@ Route::get('/', function () {
             'news',
             'shop'
         ],
+
+        'db' => config('db'),
     ];
 
     return view('home', $data);
