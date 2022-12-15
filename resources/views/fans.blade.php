c9b686cfe7e07665dc416125a6dc0002d8d17796
FabrizioMisseri <misserifabrizio@gmail.com>
2022-12-15 17:24:03 +0100

    adds institutional-pages

---
+++ resources/views/fans.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts/app')
+
+@section('content')
+    <section>
+        <h2>
+            ciao sono fans
+        </h2>
+        <h4>
+            <a href=" {{ route('home') }} " style="display: inline-block">
+                torna alla home
+            </a>
+        </h4>
+    </section>
+@endsection
